7d293e67152041184163e3a11a63b8264b540ae2
Armin DM <arminmaher@gmail.com>
2021-02-01 20:58:35 +0330

    adding forms.py

---
+++ Yektanet/advertiser_management/forms.py
@@ -0,0 +1,10 @@
+from django import forms
+
+from .models import Advertiser
+
+
+class CreateAd(forms.Form):
+    advertiser_id = forms.ChoiceField(choices=Advertiser.getChoiceList(), label="Advertiser ID")
+    image = forms.ImageField(label="Image")
+    title = forms.CharField(max_length=100, label="Title")
+    link = forms.URLField(label='URL')
