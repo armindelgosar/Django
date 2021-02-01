4317ccb0ee4d69e7a4798b7858f7efbb13308212
Armin DM <arminmaher@gmail.com>
2021-02-01 21:40:43 +0330

    some changes in forms

--- Yektanet/advertiser_management/views.py
+++ Yektanet/advertiser_management/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse, Http404
+from .forms import MakeAd
+from .models import Advertiser, Ad
 
 
 def index(request):
     text = "This is my first django project in Yektanet!"
     return HttpResponse(text)
+
