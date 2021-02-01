e99a4a07922e407cff1ebb52c1be6b3bf6b86e19
Armin DM <arminmaher@gmail.com>
2021-02-01 18:57:55 +0330

    creating models.py with models similar to part2

--- Yektanet/advertiser_management/models.py
+++ Yektanet/advertiser_management/models.py
@@ -1,3 +1,38 @@
 from django.db import models
 
-# Create your models here.
+
+class BaseAdvertising(models.Model):
+    clicks = models.IntegerField(default=0)
+    views = models.IntegerField(default=0)
+
+
+class Advertiser(BaseAdvertising):
+    name = models.CharField(max_length=100)
+    id = models.AutoField(primary_key=True)
+
+    def __str__(self):
+        return str(self.id)
+
+    @staticmethod
+    def getChoiceList():
+        list1 = []
+        for advertiser in Advertiser.objects.all():
+            list1.append((advertiser.id, advertiser.name + " id: " + str(advertiser.id)))
+        return list1
+
+    @staticmethod
+    def get_by_id(id):
+        for advertiser in Advertiser.objects.all():
+            if advertiser.id == id:
+                return advertiser
+
+
+class Ad(BaseAdvertising):
+    id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=100)
+    link = models.CharField(max_length=200)
+    image = models.ImageField(upload_to="images", default="")
+    advertiser = models.ForeignKey(Advertiser, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return str(self.id)
