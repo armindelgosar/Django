75aacb77102e0046e49a7666efb1b48da73e51d8
Armin DM <arminmaher@gmail.com>
2021-02-03 15:16:08 +0330

    fixing urls.py

--- Yektanet/Yektanet/urls.py
+++ Yektanet/Yektanet/urls.py
@@ -18,6 +18,9 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('advertiser_management/', include('advertiser_management.urls')),
+    path('advertiser_management/', include('advertiser_management.urls', namespace="advertiser_management")),
     path('admin/', admin.site.urls),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
--- Yektanet/advertiser_management/urls.py
+++ Yektanet/advertiser_management/urls.py
@@ -1,8 +1,5 @@
 from django.urls import path
 from . import views
-from django.shortcuts import get_object_or_404
-from django.views.generic.base import RedirectView
-from .models import Ad
 
 app_name = "advertiser_management"
 urlpatterns = [
@@ -11,5 +8,5 @@ urlpatterns = [
     path('create_ad', views.create_ad, name='create_ad'),
     path('<int:pk>/',
          views.AdRedirectView.as_view()
-    , name='ad-redirect'),
+         , name='ad-redirect'),
 ]
