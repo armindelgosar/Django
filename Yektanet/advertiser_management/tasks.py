919faf719b384d4c484bd5bf9effcdbb445b2ed1
Armin DM <arminmaher@gmail.com>
2021-02-16 15:50:41 +0330

    fix: Fix celery tasks

--- Yektanet/advertiser_management/tasks.py
+++ Yektanet/advertiser_management/tasks.py
@@ -6,28 +6,28 @@ from advertiser_management.models import Ad
 app = Celery('tasks', broker='pyamqp://guest@localhost//')
 
 
-@shared_task
+@app.task
 def count_total_views_per_hour():
     list1 = Ad.objects.all()
     for ad in list1:
         ad.make_hourly_views()
 
 
-@shared_task
+@app.task
 def count_total_clicks_per_hour():
     list1 = Ad.objects.all()
     for ad in list1:
         ad.make_hourly_clicks()
 
 
-@shared_task
+@app.task
 def count_total_views_per_day():
     list1 = Ad.objects.all()
     for ad in list1:
         ad.make_daily_views()
 
 
-@shared_task
+@app.task
 def count_total_clicks_per_day():
     list1 = Ad.objects.all()
     for ad in list1:
